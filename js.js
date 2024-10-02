9e37a2dea17c5d59e4d6610353b9d4253755c124
rajeshracha6 <rajeshracha752@gmail.com>
2024-10-02 12:27:50 +0530

    commit

---
+++ js.js
@@ -0,0 +1,13 @@
+// arr =[202,89,112,89];
+// result = []
+// for(i of arr){
+//     istr = String(i)
+//     emp=[]
+//     for(j of istr){
+//         if(!emp.includes(j)){
+//             emp.push(j)
+//         }
+//     }
+//     result.push(istr.length==emp.length)
+// }
+// console.log(result)
